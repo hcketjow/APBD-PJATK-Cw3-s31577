012dfa270f5f60990344515eb8ab6988d628cc67
hcketjow <hcketjow@gmail.com>
2026-04-28 20:19:32 +0200

    Room and reservation mappers added

--- TutorialCenter/Mappers/ReservationMappingExtensions.cs
+++ TutorialCenter/Mappers/ReservationMappingExtensions.cs
@@ -1,6 +1,60 @@
-﻿namespace TutorialCenter.Mappers;
+﻿using TutorialCenter.DTOs;
+using TutorialCenter.Models;
 
-public class ReservationMappingExtensions
-{
-    
+namespace TutorialCenter.Mappers;
+
+public static class ReservationMappingExtensions {
+    public static Reservation ToDomain(this ReservationDto reservationDto)
+    {
+        return new Reservation
+        {
+            Id = reservationDto.Id,
+            RoomId = reservationDto.RoomId,
+            OrganizerName = reservationDto.OrganizerName,
+            Topic =  reservationDto.Topic,
+            StartTime = reservationDto.StartTime,
+            EndTime = reservationDto.EndTime,
+            ReservationStatus = Reservation.Status.planned
+        };
+    }
+
+    public static Reservation ToDomain(this CreateReservationDto createReservation)
+    {
+        return new Reservation
+        {
+            RoomId = createReservation.RoomId,
+            OrganizerName = createReservation.OrganizerName,
+            Topic =  createReservation.Topic,
+            StartTime = createReservation.StartTime,
+            EndTime = createReservation.EndTime,
+            ReservationStatus = Reservation.Status.planned
+        };
+    }
+
+    public static Reservation ToDomain(this UpdateReservationDto updateReservationDto)
+    {
+        return new Reservation
+        {
+            RoomId = updateReservationDto.RoomId,
+            OrganizerName = updateReservationDto.OrganizerName,
+            Topic =  updateReservationDto.Topic,
+            StartTime = updateReservationDto.StartTime,
+            EndTime = updateReservationDto.EndTime,
+            ReservationStatus = Reservation.Status.planned
+        };
+    }
+
+    public static ReservationDto ToDto(this Reservation reservation)
+    {
+        return new ReservationDto
+        {
+            Id = reservation.Id,
+            RoomId = reservation.RoomId,
+            OrganizerName = reservation.OrganizerName,
+            Topic =  reservation.Topic,
+            StartTime = reservation.StartTime,
+            EndTime = reservation.EndTime,
+            ReservationStatus = ReservationStatus.Planned
+        };
+    }
 }
--- TutorialCenter/Mappers/RoomMappingExtensions.cs
+++ TutorialCenter/Mappers/RoomMappingExtensions.cs
@@ -1,6 +1,59 @@
-﻿namespace TutorialCenter.Mappers;
+﻿using TutorialCenter.DTOs;
+using TutorialCenter.Models;
 
-public class RoomMappingExtensions
-{
+namespace TutorialCenter.Mappers;
+
+public static class RoomMappingExtensions {
+    public static Room ToDomain(this RoomDto roomDto)
+    {
+        return new Room
+        {
+            Id = roomDto.Id,
+            Name = roomDto.Name,
+            BuildingCode =  roomDto.BuildingCode,
+            Floor =  roomDto.Floor,
+            Capacity = roomDto.Capacity,
+            HasProjector = roomDto.HasProjector,
+            IsActive =  roomDto.IsActive
+        };
+    }
     
+    public static Room ToDomain(this CreateRoomDto createRoomDto)
+    {
+        return new Room
+        {
+            Name = createRoomDto.Name,
+            BuildingCode =  createRoomDto.BuildingCode,
+            Floor =  createRoomDto.Floor,
+            Capacity = createRoomDto.Capacity,
+            HasProjector = createRoomDto.HasProjector,
+            IsActive =  createRoomDto.IsActive
+        };
+    }
+    
+    public static Room ToDomain(this UpdateRoomDto updateRoomDto)
+    {
+        return new Room
+        {
+            Name = updateRoomDto.Name,
+            BuildingCode =  updateRoomDto.BuildingCode,
+            Floor =  updateRoomDto.Floor,
+            Capacity = updateRoomDto.Capacity,
+            HasProjector = updateRoomDto.HasProjector,
+            IsActive =  updateRoomDto.IsActive
+        };
+    }
+    
+    public static RoomDto ToDo(this Room room)
+    {
+        return new RoomDto {
+            Id = room.Id,
+            Name = room.Name,
+            BuildingCode =  room.BuildingCode,
+            Floor =  room.Floor,
+            Capacity = room.Capacity,
+            HasProjector = room.HasProjector,
+            IsActive =  room.IsActive
+        };
+    }
 }
